9ce7dd5c1bb8e487c012e45fbe6f40fafd97192a
Walisson Mariano <marianowalisson98@gmail.com>
2024-10-15 19:59:02 -0300

    feat: create entity cart

---
+++ src/cart/entities/cart.entity.ts
@@ -0,0 +1,15 @@
+import { Column, CreateDateColumn, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+export class Cart {
+    @PrimaryGeneratedColumn('rowid')
+    id: number;
+
+    @Column({ name: 'user_id', nullable: false })
+    name: string;
+
+    @CreateDateColumn({ name: 'created_at' })
+    createdAt: Date;
+
+    @UpdateDateColumn({ name: 'update_at' })
+    updatedAt: Date;
+}
